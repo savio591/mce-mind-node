c0692628e4b4722d92597f9ce6dd663a0147a3ce
savio591 <savio591@hotmail.com>
2021-08-04 18:44:02 -0300

    feat: lesson 1 - Auth, Database Model  and Controllers

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '0b064b3861b2f7dce9a7885d84997377',
+  expiresIn: '7d',
+};
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  password: 'root',
+  database: 'case_mce',
+  define: {
+    timestamps: true, // garante que será criado um atributo: created_at e updated_at na tabela do banco de dados.
+    underscored: true, // permite o ORM criar nome de tabelas como products_item
+    underscoredAll: true, // permite o ORM criar nome dos atributos com caixa baixa e _ em vez de camelCase, pois esse é a convenção de escrita no banco de dados
+  },
+};
